5d7ee621fcf72c43ffcfbed1df226c825c2198a4
Eric C. Leão <ericcleao@gmail.com>
2019-08-10 15:48:55 -0300

    WIP: Add databinding to the project
    Signed-off-by: Eric C. Leão <ericcleao@gmail.com>

--- app/src/main/java/io/github/wellingtoncosta/cursoandroidcongressodeti/application/ui/SelectContactActivity.kt
+++ app/src/main/java/io/github/wellingtoncosta/cursoandroidcongressodeti/application/ui/SelectContactActivity.kt
@@ -2,13 +2,19 @@ package io.github.wellingtoncosta.cursoandroidcongressodeti.application.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil.setContentView
 import io.github.wellingtoncosta.cursoandroidcongressodeti.R
+import io.github.wellingtoncosta.cursoandroidcongressodeti.databinding.SelectContactActivityBinding
 
 class SelectContactActivity : AppCompatActivity() {
 
+    private lateinit var mBinding: SelectContactActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lista_contatos)
+
+        mBinding = setContentView(this, R.layout.select_contact_activity)
     }
+
 
 }
