166c31e88c3a42636598608b70415d6c64a6253b
Eric C. Leão <ericcleao@gmail.com>
2019-08-11 14:24:53 -0300

    Refactory contact fuel api data source
    - Remove method body and use kotlin instead
    
    Signed-off-by: Eric C. Leão <ericcleao@gmail.com>

--- app/src/main/java/io/github/wellingtoncosta/cursoandroidcongressodeti/services/remote/contact/ContactFuelApiDataSource.kt
+++ app/src/main/java/io/github/wellingtoncosta/cursoandroidcongressodeti/services/remote/contact/ContactFuelApiDataSource.kt
@@ -1,30 +1,28 @@
 package io.github.wellingtoncosta.cursoandroidcongressodeti.services.remote.contact
 
+import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.gson.responseObject
-import com.github.kittinunf.fuel.httpGet
 import io.github.wellingtoncosta.cursoandroidcongressodeti.domain.exceptions.runtimeexception.NetworkException
 import io.github.wellingtoncosta.cursoandroidcongressodeti.services.remote.contact.responses.ContactResponse
 
 class ContactFuelApiDataSource : ContactApiDataSource {
 
-    override fun getAllContacts(): List<ContactResponse> {
-        val (_, response, result) = "/contacts".httpGet().responseObject<List<ContactResponse>>()
-
-        return when (response.statusCode) {
-            200, 304 -> result.get()
-            else -> throw NetworkException()
+    override fun getAllContacts() = Fuel.get("/contacts")
+        .responseObject<List<ContactResponse>>().let {
+            when (it.second.statusCode) {
+                200, 304 -> it.third.get()
+                else -> throw NetworkException()
+            }
         }
-    }
 
-    override fun getContactById(contactId: Int): ContactResponse? {
-        val (_, response, result) = "/contacts/$contactId".httpGet().responseObject<ContactResponse>()
-
-        return when (response.statusCode) {
-            200, 304 -> result.get()
-            404 -> null
-            else -> throw NetworkException()
+    override fun getContactById(contactId: Int) = Fuel.get("/contacts/$contactId")
+        .responseObject<ContactResponse>().let {
+            when (it.second.statusCode) {
+                200, 304 -> it.third.get()
+                404 -> null
+                else -> throw NetworkException()
+            }
         }
-    }
 
 
 }
